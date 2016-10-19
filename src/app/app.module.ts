727dc6bf3dfc2c15a1bd37e7b16bdaa9b6f985ec
Kim Kern <kim.kern@fu-berlin.de>
2016-10-20 01:21:09 +0200

    add app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppRoutingComponent } from './app-routing/app-routing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRoutingComponent
   ],
   imports: [
     BrowserModule,
