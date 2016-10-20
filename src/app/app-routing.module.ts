289c9fbc6f87ab331277b005d33259ea2a0add52
Kim Kern <kim.kern@fu-berlin.de>
2016-10-20 14:17:22 +0200

    add routes for match/edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'match-browser', pathMatch: 'full' }
+  { path: '', redirectTo: 'matches', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/match/match-routing.module.ts
+++ src/app/match/match-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MatchBrowserComponent } from './browser/match-browser.component';
+import { MatchEditComponent }  from './edit/match-edit.component';
 
 export const routes: Routes = [
-  { path: 'match-browser', component: MatchBrowserComponent }
+  { path: 'matches', component: MatchBrowserComponent },
+  { path: 'matches/edit', component: MatchEditComponent },
 ];
 
 @NgModule({
